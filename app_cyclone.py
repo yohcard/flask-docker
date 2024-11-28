8d6ebc08a291a995a06089036bda9dd8cb9e7472
yohcard <cardisyohan@gmail.com>
2024-11-28 15:36:46 +0100

    youpi6

--- app_cyclone.py
+++ app_cyclone.py
@@ -22,3 +22,4 @@ class CyclonesTests(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+  
--- app_test.py
+++ app_test.py
@@ -22,3 +22,4 @@ class Tests(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+    
